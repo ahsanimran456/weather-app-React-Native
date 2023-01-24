80f594b8e26d72d77739cd89cd5bc7f88c522a2a
Ahsan Imran <111527387+ahsanimran456@users.noreply.github.com>
2023-01-24 13:19:00 -0800

    Add files via upload

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import {
   SafeAreaView,
@@ -9,31 +8,19 @@ import {
   useColorScheme,
   View,
 } from 'react-native';
-import Router from './Router/Router';
-function App()  {
-
+import { Provider } from 'react-redux';
+import Store from './Redux/Store';
+import Navigations from './Navgigations/Navigation';
+function App() {
   return (
-    <Router/>
-  )
+    <Provider store={Store}>
+      <Navigations />
+    </Provider>
+  );
 }
 
-// const styles = StyleSheet.create({
-//   sectionContainer: {
-//     marginTop: 32,
-//     paddingHorizontal: 24,
-//   },
-//   sectionTitle: {
-//     fontSize: 24,
-//     fontWeight: '600',
-//   },
-//   sectionDescription: {
-//     marginTop: 8,
-//     fontSize: 18,
-//     fontWeight: '400',
-//   },
-//   highlight: {
-//     fontWeight: '700',
-//   },
-// });
+const styles = StyleSheet.create({
+
+});
 
 export default App;
